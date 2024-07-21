2fe9a16853a231d947787be7db6744d577ca083d
mar-alv <mar.alv.cod@gmail.com>
2024-07-21 11:28:03 -0300

    feat: handling errors

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import fastify from 'fastify'
 import cookie from '@fastify/cookie'
 
+import { errorHandler } from './error-handler'
 import { usersRoutes } from './routes/users'
 
 export const app = fastify()
 
 app.register(cookie)
+
+app.setErrorHandler(errorHandler)
+
 app.register(usersRoutes, {
   prefix: 'users',
 })
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -2,16 +2,27 @@ import { FastifyInstance } from 'fastify'
 import { randomUUID } from 'node:crypto'
 import { z } from 'zod'
 
+import { checkIfSessionIdExists } from '../middlewares/check-if-session-id-exists'
 import { knex } from '../database'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/', async (request, reply) => {
     const createUserBodySchema = z.object({
-      name: z.string().min(3),
-      age: z.number().min(12),
-      height: z.number().min(140),
-      weight: z.number().min(30),
-      sex: z.enum(['masculine', 'feminine']),
+      name: z
+        .string()
+        .min(3, { message: 'Name must be at least 3 characters long' }),
+      age: z
+        .number()
+        .min(12, { message: 'Age must be at least 12 years old' })
+        .max(110, { message: 'Age must be no more than 110 years old' }),
+      height: z
+        .number()
+        .min(130, { message: 'Height must be at least 130 cm' })
+        .max(270, { message: 'Height must be no more than 270 cm' }),
+      weight: z.number().min(30, { message: 'Weight must be at least 30 kg' }),
+      sex: z.enum(['masculine', 'feminine'], {
+        message: 'Please select either "masculine" or "feminine" as your sex',
+      }),
     })
 
     const { age, height, name, sex, weight } = createUserBodySchema.parse(
@@ -40,5 +51,9 @@ export async function usersRoutes(app: FastifyInstance) {
     })
 
     return reply.status(201).send()
+  })
+
+  app.get('/', async (request, reply) => {
+    return await knex('users').select()
   })
 }
